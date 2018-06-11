49b193b5451a0a702df25436b3a14c3c12e9b0f1
lionthai <takw2477>
2018-06-11 16:54:18 +0700

    test

---
+++ system/views/routing/views/routing.php
@@ -0,0 +1,38 @@
+<div class="row" >
+    <table style="width: 100%;" >
+        <tr>
+            <td ></td>
+            <td style="text-align: right;">
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#">
+                    Production Line +
+                </button>   
+            </td>
+        </tr>
+    </table>
+</div>
+
+
+<div class="row">
+    <table class="table table-condensed">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Tools</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>
+                         
+                        </td>
+                    </tr>
+               
+           
+        </tbody>
+    </table>
+</div>
+
