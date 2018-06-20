cf1ecb5c1ff046a09ec208c53ed613f66f18c89d
takw2477 <birdsurachai@gmail.com>
2018-06-20 23:28:26 +0700

    checksheet

--- system/views/checksheet/query/ajaxSend.php
+++ system/views/checksheet/query/ajaxSend.php
@@ -14,9 +14,17 @@ INNER JOIN `subproductionline` ON  `subchecksheet`.`subproductionlineID`= `subpr
 $result = mysqli_query($connection, $query);
 while ($row = mysqli_fetch_array($result)) {
     $subproductionlineID = $row['subproductionlineID'];
+    $query="SELECT * FROM `subproductionline` WHERE `id` ='$subproductionlineID'";
+    $result1=  mysqli_query($connection, $query);
+    $row1=  mysqli_fetch_array($result1);
     $speed=$row['speed'];
-    $query="UPDATE `subchecksheet` SET `speed` ='$speed'WHERE  `checksheet` ='$checksheetId' and `subproductionlineID` ='$subproductionlineID'";
-     mysqli_query($connection, $query);
+    $reject_total=$row1['reject_total'];
+    $free_total=$row1['free_total'];
+    $actual_total=$row1['actual_total'];
+    $target=$row1['target'];
+    $query=" UPDATE `subchecksheet` SET  `target` ='$target',`actual_total`='$actual_total',`free_total`='$free_total',`reject_total`='$reject_total',`speed`='$speed'   WHERE      `checksheet` ='$checksheetId' and `subproductionlineID` ='$subproductionlineID'";
+ 
+    mysqli_query($connection, $query);
     $query = "UPDATE `subproductionline` SET  `speed`='0',`reject_total`='0' ,`free_total`='0' ,`actual_total`='0' ,`target`  ='0' ,`status`='0' WHERE `id` ='$subproductionlineID'";
     mysqli_query($connection, $query);
 }
