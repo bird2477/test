8876f1dd502cf2f81ca14f366359b9491e8ab2b9
takw2477 <birdsurachai@gmail.com>
2018-06-23 21:50:59 +0700

    update <link rel="stylesheet" href="../../../../css/bootstrap.css"  crossorigin="anonymous"> <script src="../../../../js/jquery.min.js" crossorigin="anonymous"></script> <script src="../../../../js/bootstrap.js"  crossorigin="anonymous"></script>

--- system/views/routing/views/subproductionline.php
+++ system/views/routing/views/subproductionline.php
@@ -4,11 +4,10 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-    <head> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <head> 
+        <link rel="stylesheet" href="../../../../css/bootstrap.css"  crossorigin="anonymous">
+        <script src="../../../../js/jquery.min.js" crossorigin="anonymous"></script>
+        <script src="../../../../js/bootstrap.js"  crossorigin="anonymous"></script>
 
         <script >
             $(document).ready(function () {
@@ -19,7 +18,7 @@ session_start();
                     var capability = $("#capability").val();
                     var model = $("#model").val();
                     var brandname = $("#brandname").val();
-                    var dataString = "productionline=" + productionline + "&name=" + name+ "&type=" + type+ "&capability=" + capability+ "&model=" + model+ "&brandname=" + brandname;
+                    var dataString = "productionline=" + productionline + "&name=" + name + "&type=" + type + "&capability=" + capability + "&model=" + model + "&brandname=" + brandname;
                     $.ajax({data: dataString, url: "../../routing/query/ajaxAddSubMachine.php", cache: false, type: 'POST', success: function (data, textStatus, jqXHR) {
                             if (data == 1) {
                                 window.location.reload();
@@ -105,7 +104,7 @@ session_start();
                                     </div>
                                 </div>
                             </div>
-                              <div class="mb-3">
+                            <div class="mb-3">
                                 <label for="model">Model</label>
                                 <div class="input-group">
 
@@ -115,7 +114,7 @@ session_start();
                                     </div>
                                 </div>
                             </div>
-                             <div class="mb-3">
+                            <div class="mb-3">
                                 <label for="brandname">Brand Name</label>
                                 <div class="input-group">
 
