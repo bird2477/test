b92b1b211f876f81adbda4a3a1616b5d3818438d
takw2477 <birdsurachai@gmail.com>
2018-06-25 18:39:14 +0700

    customer

--- system/views/customer/query/nameENautocomplate.php
+++ system/views/customer/query/nameENautocomplate.php
@@ -1,14 +1,15 @@
 <?php	
        include '../../../../config/database.php';
 
-	$keyword = strval($_POST['companynameEN']);
+	$keyword = strval($_POST['search_param']);
 	$search_param = "%{$keyword}%";
-	$query ="SELECT `companynameEN` FROM `customer` WHERE `companynameEN` like  '$search_param'";
+	$query ="SELECT `companynameEN`,`companynameTH`  FROM `customer` WHERE `companynameEN` like  '$search_param'";
         $result= mysqli_query($connection, $query);
 	
 	if (mysqli_num_rows($result)>0) {
 		while($row =  mysqli_fetch_assoc($result)) {
 		$countryResult[] = $row["companynameEN"];
+		$countryResult[] = $row["companynameTH"];
 		}
 		echo json_encode($countryResult);
 	}
