7c98367b1d1129607d587fd89f6577bbefe00cba
takw2477 <birdsurachai@gmail.com>
2018-07-07 11:20:28 +0700

    grgrg

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ if (isset($_SESSION['id'])) {
 
             <h1 class="h3 mb-3 font-weight-normal">Please Log in</h1>
             <label for="username" class="sr-only">Username</label>
-            <input type="text" id="username" class="form-control" name="username" placeholder="Username" required="" autofocus="">
+            <input type="text" id="username" class="form-control" autocomplete="off" name="username" placeholder="Username" required="" autofocus="">
             <label for="password" class="sr-only">Password</label>
-            <input type="password" id="password" class="form-control" name="password" placeholder="Password" required="">
+            <input type="password" id="password" class="form-control" autocomplete="off" name="password" placeholder="Password" required="">
 
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 
